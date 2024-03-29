d7b4f0a659b436eda4398e767ae7a93786e203a9
Vaibhav Sutar <apple@Apples-MacBook-Air.local>
2024-03-29 16:16:32 +0530

    Sizes

---
+++ lib/common/sizes.dart
@@ -0,0 +1,68 @@
+// height 938
+// width 430
+
+// Height
+
+const double height40 = 23.45;
+const double height30 = 31.2666666667;
+const double height34 = 27.5882352941;
+const double height55 = 17.0545454545;
+const double height60 = 15.6333333333;
+const double height10 = 93.8;
+const double height16 = 58.625;
+const double height5 = 187.6;
+const double height20 = 46.9;
+const double height22 = 42.6363636364;
+const double height46 = 20.3913043478;
+const double height64 = 14.65625;
+const double height70 = 13.4;
+const double height44 = 21.3181818182;
+const double height74 = 12.6756756757;
+const double height90 = 10.4222222222;
+const double height96 = 9.7708333333;
+const double height242 = 3.8760330579;
+const double height200 = 4.69;
+const double height11 = 85.2727272727;
+const double height13 = 72.1538461538;
+const double height48 = 19.5416666667;
+const double height66 = 14.2121212121;
+const double height65 = 14.4307692308;
+const double height76 = 12.3421052632;
+const double height470 = 1.9957446809;
+const double height144 = 6.5138888889;
+const double height250 = 3.752;
+const double height100 = 9.38;
+const double height27 = 34.7407407407;
+
+// Width
+
+const double width74 = 5.8108108108;
+const double width13 = 33.0769230769;
+const double width4 = 107.5;
+const double width10 = 43;
+const double width12 = 35.8333333333;
+const double width14 = 30.7142857143;
+const double width18 = 23.8888888889;
+const double width16 = 26.875;
+const double width20 = 21.5;
+const double width24 = 17.9166666667;
+const double width26 = 16.5384615385;
+const double width28 = 15.3571418571;
+const double width30 = 14.3333333333;
+const double width34 = 12.6470588235;
+const double width38 = 11.3157894737;
+const double width70 = 6.1428571429;
+const double width40 = 10.75;
+const double width19 = 22.6315789474;
+const double width48 = 8.9583333333;
+const double width44 = 9.7727272727;
+const double width55 = 7.8181818182;
+const double width35 = 12.2857142857;
+const double width90 = 4.7777777778;
+const double width98 = 4.387755102;
+const double width200 = 2.15;
+const double width300 = 1.4333333333;
+const double width65 = 6.6153846154;
+const double width32 = 13.4375;
+const double width45 = 9.5555555556;
+const double width54 = 7.962962963;
