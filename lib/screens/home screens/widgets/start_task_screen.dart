e6b80d98abf80effe17362137b46b2c966aff543
VaibhavSutar <apple@Apples-MacBook-Air.local>
2024-04-21 23:45:27 +0530

    New Late Timer

--- lib/screens/home screens/widgets/start_task_screen.dart
+++ lib/screens/home screens/widgets/start_task_screen.dart
@@ -107,7 +107,8 @@ class _StartTaskScreenState extends State<StartTaskScreen> {
     var TestinEmail = "pankaj.kumar1312@yahoo.com";
     var defaultEmail = "tacttikofficial@gmail.com";
     var ClientName = await fireStoreService.getClientName(widget.ShiftClientID);
-    // emails.add(ClientEmail!);
+    emails.add(ClientEmail!);
+    emails.add(defaultEmail);
     // emails.add("sutarvaibhav37@student.sfit.ac.in");
     // emails.add("sutarvaibhav37@gmail.com");
     var testEmail3 = "sales@tpssolution.com";
@@ -120,11 +121,10 @@ class _StartTaskScreenState extends State<StartTaskScreen> {
     await fireStoreService.fetchPatrolData(widget.ShiftId, widget.EmployeId);
 
     if (ClientEmail != null && AdminEmail != null) {
-      // emails.add(AdminEmail);
-
-      // emails.add(TestinEmail);
+      emails.add(AdminEmail);
+      emails.add(TestinEmail);
       // emails.add(testEmail3);
-      emails.add(testEmail4);
+      // emails.add(testEmail4);
 
       await sendShiftEmail(
         ClientName,
