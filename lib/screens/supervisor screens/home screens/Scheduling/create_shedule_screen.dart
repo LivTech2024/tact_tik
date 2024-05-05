b4bedf0e1c2fa08969e807d6e1a2ddb85c76b85e
Swastik <swastikbthiramdas@gmail.com>
2024-05-05 20:26:01 +0530

    new updated

--- lib/screens/supervisor screens/home screens/Scheduling/create_shedule_screen.dart
+++ lib/screens/supervisor screens/home screens/Scheduling/create_shedule_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_places_flutter/google_places_flutter.dart';
 import 'package:google_places_flutter/model/prediction.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'package:tact_tik/common/sizes.dart';
 import 'package:tact_tik/common/widgets/button1.dart';
 import 'package:tact_tik/fonts/inter_bold.dart';
@@ -12,102 +13,6 @@ import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 
 import '../widgets/set_details_widget.dart';
 import 'select_guards_screen.dart';
-
-class MultiDateSelectionDialog extends StatefulWidget {
-  @override
-  _MultiDateSelectionDialogState createState() =>
-      _MultiDateSelectionDialogState();
-}
-
-class _MultiDateSelectionDialogState extends State<MultiDateSelectionDialog> {
-  List<DateTime> selectedDates = [];
-
-  @override
-  Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
-    final double width = MediaQuery.of(context).size.width;
-    return AlertDialog(
-      title: InterMedium(
-        text: 'Select Dates',
-        color: color2,
-        fontsize: width / width20,
-      ),
-      content: Container(
-        width: double.minPositive,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              // Display selected dates
-              if (selectedDates.isNotEmpty)
-                Column(
-                  children: selectedDates
-                      .map((date) => ListTile(
-                            title: Text(date.toString()),
-                            trailing: IconButton(
-                              icon: Icon(Icons.delete),
-                              onPressed: () {
-                                setState(() {
-                                  selectedDates.remove(date);
-                                });
-                              },
-                            ),
-                          ))
-                      .toList(),
-                ),
-              // Date picker for selecting new dates
-              ElevatedButton(
-                onPressed: () {
-                  _selectDate(context);
-                },
-                child: InterMedium(
-                  text: 'Select Date',
-                  color: Primarycolor,
-                  fontsize: width / width16,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      actions: <Widget>[
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop(selectedDates);
-          },
-          child: InterMedium(
-            text: 'OK',
-            color: Primarycolor,
-            fontsize: width / width18,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? datePicked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2023),
-      lastDate: DateTime(3000),
-      builder: (BuildContext context, Widget? child) {
-        return Theme(
-          data: ThemeData.light().copyWith(
-            colorScheme: ColorScheme.dark(
-              primary: Primarycolor, // Change primary color to red
-            ),
-          ),
-          child: child!,
-        );
-      },
-    );
-    if (datePicked != null) {
-      setState(() {
-        selectedDates.add(datePicked);
-      });
-    }
-  }
-}
 
 class CreateSheduleScreen extends StatefulWidget {
   final String GuardId;
@@ -163,60 +68,58 @@ class _CreateSheduleScreenState extends State<CreateSheduleScreen> {
   TextEditingController _PhotoUploadIntervalInMinutes = TextEditingController();
   TextEditingController _Branch = TextEditingController();
   bool _isRestrictedChecked = false;
-  List<DateTime> selectedDates = []; // Define and initialize selectedDates list
-
+  List<DateTime> _selectedDates = [];
   List<Map<String, dynamic>> tasks = [];
 
   List<String> locationSuggestions = ['Location 1', 'Location 2', 'Location 3'];
   String dropdownValue = 'Other';
   List<String> tittles = [];
 
-  void _selectDate(BuildContext context) async {
-    final List<DateTime>? selectedDates = await showDialog<List<DateTime>>(
+  void _showDatePicker(BuildContext context) {
+    showDialog(
       context: context,
-      builder: (context) => MultiDateSelectionDialog(),
-    );
-
-    if (selectedDates != null) {
-      print('Selected Dates: $selectedDates');
-    }
-  }
-
-/*
-  void _selectDate(BuildContext context) async {
-    List<DateTime> selectedDates = [];
-
-    final List<DateTime>? pickedDates = await showCalendarDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2023),
-      lastDate: DateTime(3000),
-      selectableDayPredicate: (DateTime date) {
-        // Customize the predicate to allow or disallow specific dates to be selectable
-        return true; // Allow all dates to be selectable
+      builder: (BuildContext context) {
+        return Dialog(
+          child: Container(
+            width: MediaQuery.of(context).size.width - 120,
+            height: 400,
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text('Select Dates', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                SizedBox(height: 20),
+                Expanded(
+                  child: SfDateRangePicker(
+                    selectionTextStyle: TextStyle(color: Primarycolor), // Use primary color here
+                    selectionShape: DateRangePickerSelectionShape.circle,
+                    selectionColor: Primarycolor, // Use primary color here
+                    selectionRadius: 4,
+                    view: DateRangePickerView.month,
+                    selectionMode: DateRangePickerSelectionMode.multiple,
+                    initialSelectedDates: _selectedDates,
+                    onSelectionChanged: (DateRangePickerSelectionChangedArgs args) {
+                      setState(() {
+                        _selectedDates = args.value.cast<DateTime>();
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Done'),
+                ),
+              ],
+            ),
+          ),
+        );
       },
-      initialSelectionMode: SelectionMode.multi,
-      headerTextStyle: TextStyle(color: Colors.white),
-      selectedDecoration: BoxDecoration(
-        color: Color(0xFF704600),
-        shape: BoxShape.circle,
-      ),
-      todayDecoration: BoxDecoration(
-        color: Color(0xFFCBA76B),
-        shape: BoxShape.circle,
-      ),
     );
-
-    if (pickedDates != null) {
-      // User selected one or more dates
-      setState(() {
-        selectedDates.addAll(pickedDates);
-      });
-    }
-
-    print('Selected Dates: $selectedDates');
   }
-*/
 
   Future<TimeOfDay?> showCustomTimePicker(BuildContext context) async {
     TimeOfDay? selectedTime;
@@ -600,14 +503,6 @@ class _CreateSheduleScreenState extends State<CreateSheduleScreen> {
                             color: color1,
                           ),
                           SizedBox(height: height / height10),
-                          // Select Guard
-                          // SetDetailsWidget(
-                          //   useTextField: true,
-                          //   hintText: 'Shift Position',
-                          //   icon: Icons.control_camera,
-                          //   controller: _ShiftPosition,
-                          //   onTap: () {},
-                          // ),
                           Container(
                             height: height / height60,
                             padding: EdgeInsets.symmetric(
@@ -680,8 +575,11 @@ class _CreateSheduleScreenState extends State<CreateSheduleScreen> {
                                 ? 'Date'
                                 : '${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}',
                             icon: Icons.date_range,
-                            onTap: () => _selectDate(context),
+                            onTap: () => _showDatePicker(context),
                           ),
+
+
+
                           // Seperate Time
                           SetDetailsWidget(
                             hintText: selectedTime == null
