74664e9e43a7db0b788edb8cd68517decf03c76c
Ashu Yadav <aashukumaryadav1@gmail.com>
2024-05-08 21:32:48 +0530

    fix 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tact_tik/screens/SideBar%20Screens/employment_letter.dart';
 import 'package:tact_tik/screens/SideBar%20Screens/profile_screen.dart';
 import 'package:tact_tik/screens/authChecker/authChecker.dart';
+import 'package:tact_tik/screens/feature%20screens/post_order.dart/post_order_screen.dart';
+import 'package:tact_tik/screens/feature%20screens/task/task_feature_screen.dart';
 import 'package:tact_tik/screens/feature%20screens/visitors/visitors.dart';
 
 import 'package:tact_tik/screens/get%20started/getstarted_screen.dart';
@@ -18,6 +20,7 @@ import 'package:tact_tik/screens/feature%20screens/petroling/patrolling.dart';
 import 'package:tact_tik/screens/feature%20screens/petroling/patrolling.dart';
 import 'package:tact_tik/screens/home%20screens/home_screen.dart';
 import 'package:tact_tik/screens/home%20screens/message%20screen/message_screen.dart';
+import 'package:tact_tik/screens/supervisor%20screens/features%20screens/post_order_screen.dart';
 import 'package:tact_tik/screens/supervisor%20screens/home%20screens/s_home_screen.dart';
 import 'package:tact_tik/screens/supervisor%20screens/patrol_logs.dart';
 import 'package:tact_tik/screens/view/app_view.dart';
