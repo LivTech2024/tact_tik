94892461064b9c46943c8080ca8a3674ea157aab
Swastik <swastikbthiramdas@gmail.com>
2024-05-20 13:08:40 +0530

    updates

--- lib/fonts/roboto_medium.dart
+++ lib/fonts/roboto_medium.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class RobotoMedium extends StatelessWidget {
-  const RobotoMedium({super.key, this.fontsize, required this.text, this.color});
+  const RobotoMedium({super.key, this.fontsize, required this.text, this.color, this.maxLines});
 
   final double? fontsize;
   final String text;
   final Color? color;
+  final int? maxLines;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +16,8 @@ class RobotoMedium extends StatelessWidget {
       text,
       style:
       GoogleFonts.roboto(fontWeight: FontWeight.w500, fontSize: fontsize , color: color),
+      overflow: TextOverflow.clip,
+      maxLines: maxLines,
     );
   }
 }
