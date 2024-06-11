0c60dca4138f9a437b8760e77b138e20219f28eb
noobiethe13 <noobiethe13@users.noreply.github.com>
2024-06-11 17:31:58 +0530

    Client Dar

--- lib/screens/client screens/DAR/client_open_dar_screen.dart
+++ lib/screens/client screens/DAR/client_open_dar_screen.dart
@@ -1,13 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
 import 'package:tact_tik/fonts/inter_bold.dart';
 import 'package:tact_tik/fonts/inter_regular.dart';
 import 'package:tact_tik/fonts/inter_semibold.dart';
 import 'package:tact_tik/utils/colors.dart';
 
-class ClientOpenDarScreen extends StatelessWidget {
-  const ClientOpenDarScreen({super.key});
+class ClientOpenDarScreen extends StatefulWidget {
+  final String tileLocation;
+  final String tileTime;
+  final String tileDescription;
+  final String tileReportSearchId;
+  final String empName;
 
+  const ClientOpenDarScreen({
+    super.key,
+    required this.tileLocation,
+    required this.tileTime,
+    required this.tileDescription,
+    required this.tileReportSearchId,
+    required this.empName,
+  });
+
+  @override
+  State<ClientOpenDarScreen> createState() => _ClientOpenDarScreenState();
+}
+
+class _ClientOpenDarScreenState extends State<ClientOpenDarScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -23,7 +43,7 @@ class ClientOpenDarScreen extends StatelessWidget {
             },
           ),
           title: InterRegular(
-            text: "DAR Data- Nick Jones",
+            text: "Dar Data: " + widget.empName,
           ),
           centerTitle: true,
         ),
@@ -35,12 +55,12 @@ class ClientOpenDarScreen extends StatelessWidget {
               children: [
                 SizedBox(height: 30.h),
                 InterBold(
-                  text: 'Place Spot Name Goes Here..',
+                  text: widget.tileLocation == '' ? 'Not Defined' : widget.tileLocation,
                   fontsize: 18.sp,
                 ),
                 SizedBox(height: 20.h),
                 InterSemibold(
-                  text: 'Time: 00.00 - 01.00  ',
+                  text: 'Time: ' + widget.tileTime,
                   fontsize: 14.sp,
                 ),
                 SizedBox(height: 30.h),
@@ -58,8 +78,7 @@ class ClientOpenDarScreen extends StatelessWidget {
                     borderRadius: BorderRadius.circular(13.85.r),
                   ),
                   child: InterRegular(
-                    text:
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Lorem ipsum dolor sit amet\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.  ipsum dolor sit amet\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Lorem ipsum dolor sit amet....',
+                    text: widget.tileDescription,
                     fontsize: 14.sp,
                     maxLines: 80,
                   ),
@@ -74,49 +93,83 @@ class ClientOpenDarScreen extends StatelessWidget {
                   fontsize: 20.sp,
                   color: Theme.of(context).textTheme.bodySmall!.color,
                 ),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        // print("TIle Patrol Data ${TilePatrolData}");
+                widget.tileReportSearchId == 'Not Defined'
+                    ? Padding(
+                  padding: EdgeInsets.only(top: 20.h),
+                  child: InterRegular(
+                    text: 'No Reports Found',
+                    fontsize: 16.sp,
+                  ),
+                )
+                    : StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('Reports')
+                      .where('ReportSearchId',
+                      isEqualTo: widget.tileReportSearchId)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    }
+
+                    if (!snapshot.hasData) {
+                      return CircularProgressIndicator();
+                    }
+
+                    final reports = snapshot.data!.docs;
+
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: reports.length,
+                      itemBuilder: (context, index) {
+                        final report = reports[index].data() as Map<String, dynamic>;
+                        final reportData = report['ReportData'];
+                        final reportCreatedAt =
+                        (report['ReportCreatedAt'] as Timestamp).toDate();
+                        final formattedTime =
+                        DateFormat('hh:mm a').format(reportCreatedAt);
+
+                        return GestureDetector(
+                          onTap: () {
+                            // Handle report tap
+                          },
+                          child: Container(
+                            margin: EdgeInsets.only(bottom: 30.h),
+                            height: 35.h,
+                            color: Theme.of(context).cardColor,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 15.w,
+                                  height: double.infinity,
+                                  decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    borderRadius: BorderRadius.circular(10.r),
+                                  ),
+                                ),
+                                SizedBox(width: 2.w),
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      InterBold(
+                                        text: reportData,
+                                        fontsize: 12.sp,
+                                        color: Colors.white,
+                                      ),
+                                      InterBold(
+                                        text: formattedTime,
+                                        fontsize: 12.sp,
+                                        color: Colors.white,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
                       },
-                      child: Container(
-                        margin: EdgeInsets.only(bottom: 30.h),
-                        height: 35.h,
-                        color: Theme.of(context).cardColor,
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 15.w,
-                              height: double.infinity,
-                              decoration: BoxDecoration(
-                                color: Colors.red,
-                                borderRadius: BorderRadius.circular(10.r),
-                              ),
-                            ),
-                            SizedBox(width: 2.w),
-                            Expanded(
-                              child: Column(
-                                children: [
-                                  InterBold(
-                                    text: "#334AH6 Qr Missing",
-                                    fontsize: 12.sp,
-                                    color: Colors.white,
-                                  ),
-                                  InterBold(
-                                    text: "11.36pm",
-                                    fontsize: 12.sp,
-                                    color: Colors.white,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
                     );
                   },
                 ),
