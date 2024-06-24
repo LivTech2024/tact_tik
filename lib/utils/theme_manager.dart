9f12381a285ba4cc7e28e7b33b0cd3eb604e421b
noobiethe13 <noobiethe13@users.noreply.github.com>
2024-06-24 19:32:04 +0530

    Theme Fix

--- lib/utils/theme_manager.dart
+++ lib/utils/theme_manager.dart
@@ -5,23 +5,27 @@ class ThemeManager extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.dark;
   late SharedPreferences storage;
 
-  get themeMode => _themeMode;
+  ThemeMode get themeMode => _themeMode;
+
+  ThemeManager() {
+    _loadThemeMode();
+  }
 
   void toggleTheme() async {
     storage = await SharedPreferences.getInstance();
     if (_themeMode == ThemeMode.dark) {
-      storage.setBool('isDark', true);
       _themeMode = ThemeMode.light;
-    } else {
       storage.setBool('isDark', false);
+    } else {
       _themeMode = ThemeMode.dark;
+      storage.setBool('isDark', true);
     }
     notifyListeners();
   }
 
-  init() async {
+  void _loadThemeMode() async {
     storage = await SharedPreferences.getInstance();
-    bool isDark = storage.getBool('isDark') ?? true;
+    bool isDark = storage.getBool('isDark') ?? false;
     _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
     notifyListeners();
   }
