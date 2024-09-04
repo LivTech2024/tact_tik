b326c406783fe9ff7e0b284e71fe8b6168088a96
Vishhal <narkarvishhal2005@gmail.com>
2024-09-04 21:36:32 +0530

    Skeleton of Call out Page

--- lib/screens/supervisor screens/features screens/callout/s_add_callout.dart
+++ lib/screens/supervisor screens/features screens/callout/s_add_callout.dart
@@ -10,14 +10,41 @@ class SAddCallout extends StatefulWidget {
 class _SAddCalloutState extends State<SAddCallout> {
   @override
   Widget build(BuildContext context) {
+    // Width of the User's Device
+    double screenWidth = MediaQuery.sizeOf(context).width;
+
+    // Calculating the Safe Area and Height of the User's Device
+    var padding = MediaQuery.paddingOf(context);
+    double screenHeight =
+        MediaQuery.sizeOf(context).height - padding.top - padding.bottom;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("callout"),
+        centerTitle: true,
+        title: Text("Call out"),
         leading: IconButton(
             onPressed: () {
               Navigator.pop(context);
             },
             icon: Icon(Icons.arrow_back_outlined)),
+      ),
+      body: Column(
+        children: [
+          Container(
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.all(screenHeight * 0.035),
+              child: Column(
+                children: [
+                  const Text("Create Callout"),
+                  SizedBox(width: (screenHeight * 0.035)),
+                  Container(
+                    color: Colors.grey,
+                    width: screenWidth,
+                    child: Column(),
+                  )
+                ],
+              ))
+        ],
       ),
     );
   }
